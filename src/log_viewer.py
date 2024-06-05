8a413a5f3297b7b924d3be879de511016dec70ea
eugleo <wybitul.evzen@gmail.com>
2024-06-05 21:04:26 +0200

    Update viewer

--- src/log_viewer.py
+++ src/log_viewer.py
@@ -5,6 +5,7 @@ import jsonlines
 import numpy as np
 import plotly.express as px
 import polars as pl
+import sklearn.metrics as skm
 import streamlit as st
 import typer
 from polars import col as c
@@ -62,129 +63,56 @@ def display_message(msg):
                     )
 
 
-def prev_video():
-    st.session_state.video_index -= 1
+def select_true_label(label):
+    st.session_state.selected_label = label
 
 
-# Function to increment video index
-def next_video():
-    st.session_state.video_index += 1
+def ranking_matrix(predictions: pl.DataFrame):
+    labels = predictions["true_label"].unique().sort().to_list()
 
-
-# Function to update video index based on selectbox
-def select_video(index):
-    st.session_state.video_index = index
-
-
-def select_task(index):
-    st.session_state.task_index = index
-
-
-def ranking_matrix(scores: pl.DataFrame):
-    task_labels = scores["label"].unique().sort().to_list()
-
-    rankings = (
-        scores.with_columns(
-            # add a small random number to each score to break ties
-            score=c("score") + pl.Series(np.random.rand(len(scores)) * 1e-6)
-        )
-        .with_columns(
-            pl.col("score").rank("max", descending=True).over("label").alias("rank")
-        )
-        .pivot(
-            values="rank",
-            index="true_label",
-            columns="label",
-            aggregate_function="mean",
-        )
-        .sort("true_label")
-        .select(task_labels)
+    matrix = skm.confusion_matrix(
+        predictions["true_label"].to_numpy(),
+        predictions["label"].to_numpy(),
+        labels=labels,
     )
 
-    fig = px.imshow(
-        rankings.to_numpy(),
-        x=task_labels,
-        y=task_labels,
-        labels=dict(
-            x="Candidate Label (a single ranking problem)",
-            y="Video (aliased by its true label)",
-        ),
+    heatmap = px.imshow(
+        matrix,
+        x=labels,
+        y=labels,
+        labels=dict(x="Candidate Label", y="Video True Label"),
         text_auto=True,
         height=300,
-        color_continuous_scale=px.colors.sequential.Viridis_r,
+        color_continuous_scale=px.colors.sequential.Purples,
     )
-    fig.update_layout(
+    heatmap.update_layout(showlegend=False)
+
+    heatmap.update_layout(
         showlegend=False,
         coloraxis_showscale=False,
         margin=dict(l=60, r=50, t=0, b=0, pad=0),
     )
-    return fig
+
+    return heatmap
 
 
-def main(log_dir: str):
-    st.set_page_config(layout="wide")
+def task_picker(logs, scores):
+    st.sidebar.markdown("## 1) Select Task")
 
-    logs = []
-    task_dir, task_labels = Path("tasks") / "alfred", {}
-    for log in load_logs(Path(log_dir)):
-        if "task" not in log:
-            continue
-        task = log["task"]
-        if task not in task_labels:
-            # task_labels[task] = Task.from_file(
-            #     task, Path(task_dir) / f"{task}.yaml"
-            # ).labels
-            task_labels[task] = list(sorted(log["label_descriptions"].keys()))
-
-        scores = GPTModel.parse_and_cache_scores(
-            log["history"][-1]["content"], "", task_labels[task], {}
-        )
-        log["parsed_scores"] = scores
-        logs.append(log)
-
-    if "video_index" not in st.session_state:
-        st.session_state.video_index = 0
-
-    if "task_index" not in st.session_state:
-        st.session_state.task_index = 0
-
-    st.sidebar.markdown("## Select Task")
-
-    scores_df = pl.DataFrame(
-        [
-            {
-                "task": log["task"] if "task" in log else "unknown",
-                "video": log["video"],
-                "true_label": log["true_label"],
-                "label": label,
-                "score": score,
-                "model": "gpt-4o-16",
-            }
-            for log in logs
-            for label, score in log["parsed_scores"].items()
-        ]
-    )
-    scores_df = utils.rescale(scores_df, in_each="video")
+    predictions = utils.get_predictions(scores)
 
     task_performance_df = (
-        scores_df.sort("task", "video", "label")
-        .group_by("task", maintain_order=True)
-        .agg(
-            AP=pl.struct("task", "label", "score", "true_label").map(
-                partial(plots.average_precision, task_labels)
-            )
-        )
-        .explode("AP")
-        .group_by("task")
-        .agg(mAP=pl.col("AP").mean())
-        .sort("task")
+        predictions.group_by("task", "model")
+        .agg(metric=utils.compute_metric(utils.f1))
+        .sort("task", "model")
+        .drop("model")
     )
 
     event = st.sidebar.dataframe(
-        task_performance_df.to_pandas(),
+        task_performance_df,
         column_config={
             "task": st.column_config.TextColumn(width="medium"),
-            "mAP": st.column_config.ProgressColumn(
+            "metric": st.column_config.ProgressColumn(
                 min_value=0, max_value=1, format="%.2f"
             ),
         },
@@ -199,63 +127,121 @@ def main(log_dir: str):
         else task_ids[0]
     )
 
-    st.sidebar.markdown(f"Selected: {task_id}")
+    st.sidebar.markdown("#### Selected task")
+    st.sidebar.markdown(task_id)
 
-    videos = sorted([log["video"] for log in logs if log["task"] == task_id])
-    video = videos[st.session_state.video_index]
-    # video = st.sidebar.selectbox(
-    #     "Select Video", videos, index=st.session_state.video_index
-    # )
-    log = next(log for log in logs if log["task"] == task_id and log["video"] == video)
+    st.sidebar.markdown("#### Task performance overview")
 
-    st.sidebar.markdown("---")
     st.sidebar.plotly_chart(
-        ranking_matrix(scores_df.filter(c("task") == task_id)), use_container_width=True
+        ranking_matrix(predictions.filter(c("task") == task_id)),
+        use_container_width=True,
     )
 
-    left_col, right_col = st.columns([1, 3])
-    with left_col:
-        st.video(log["video"])
+    return [log for log in logs if log["task"] == task_id]
 
-        true_label = log["true_label"]
-        predicted_label = log["predicted_label"]
-        label_descriptions = log["label_descriptions"]
 
-        fig = px.bar(
-            scores_df.filter(c("task") == task_id, c("video") == video).to_pandas(),
-            x="label",
-            y="score",
+def label_picker(logs):
+    st.sidebar.markdown("## 2) Filter videos by their true label")
+
+    log = logs[0]
+    label_descriptions = log["label_descriptions"]
+
+    for i, (label, description) in enumerate(label_descriptions.items()):
+        c1, _, c2 = st.sidebar.columns([1, 4, 1])
+        label_txt = (
+            f"**{label}**" if label == st.session_state.selected_label else label
         )
-        # fig.update_yaxes(range=[0, 1])
-        fig.update_layout(showlegend=False)
-        st.plotly_chart(fig, use_container_width=True)
+        c1.markdown(label_txt)
+        c2.button("Select", key=i, on_click=select_true_label, args=(label,))
+        st.sidebar.markdown(description)
 
-        for i, (label, description) in enumerate(label_descriptions.items()):
-            color = "green" if label == true_label else "gray"
-            label_txt = f"**{label}** 👈" if label == predicted_label else label
-            c1, _, c2 = st.sidebar.columns([1, 4, 1])
-            c1.markdown(f"{label_txt}")
-            idx = [label in v for v in videos].index(True)
-            c2.button("Select", key=i, on_click=select_video, args=(idx,))
-            st.sidebar.markdown(
-                f"<span style='color:{color}'>{description}</span>",
-                unsafe_allow_html=True,
-            )
+    return [log for log in logs if log["true_label"] == st.session_state.selected_label]
 
-    with right_col:
-        with st.container(height=1200):
-            conversation = log["history"]
-            for msg in conversation:
-                display_message(msg)
 
-    # # Previous and Next buttons, going to previous/next video in the same task
+def video_picker(logs):
+    if len(logs) == 1:
+        log = logs[0]
+    else:
+        st.sidebar.markdown("## 3) Select video")
+        valid_videos = list(set(log["video"] for log in logs))
+        video = st.sidebar.selectbox("Select Video", valid_videos)
+        log = [log for log in logs if log["video"] == video][0]
+    st.session_state.selected_log = log
+    st.session_state.selected_label = log["true_label"]
+    return log
 
-    # st.sidebar.markdown("---")
-    # if st.session_state.video_index > 0:
-    #     st.sidebar.button("Previous video", on_click=prev_video)
 
-    # if st.session_state.video_index < len(videos) - 1:
-    #     st.sidebar.button("Next video", on_click=next_video)
+def initialize_state():
+    if "selected_label" not in st.session_state:
+        st.session_state.selected_label = None
+
+
+def get_scores(logs):
+    scores = pl.DataFrame(
+        [
+            {
+                "task": log["task"] if "task" in log else "unknown",
+                "video": log["video"],
+                "true_label": log["true_label"],
+                "label": label,
+                "score": score,
+                "model": "gpt-4o",
+            }
+            for log in logs
+            for label, score in log["parsed_scores"].items()
+        ]
+    )
+    scores = utils.add_rescaling(scores).filter(c("rescaling") == "v+l")
+    return scores
+
+
+def main(log_dir: str):
+    st.set_page_config(layout="wide")
+
+    initialize_state()
+
+    logs, task_labels = [], {}
+    for log in load_logs(Path(log_dir)):
+        # if "task" not in log:
+        #     continue
+        labels = list(sorted(log["label_descriptions"].keys()))
+        task_labels.setdefault(log["task"], labels)
+
+        scores = GPTModel.parse_and_cache_scores(
+            log["history"][-1]["content"], "", task_labels[log["task"]], {}
+        )
+        log["parsed_scores"] = scores
+        logs.append(log)
+
+    scores = get_scores(logs)
+
+    logs = task_picker(logs, scores)
+    if logs:
+        logs = label_picker(logs)
+        if logs:
+            log = video_picker(logs)
+
+            left_col, right_col = st.columns([1, 3])
+            with left_col:
+                st.video(log["video"])
+
+                print(
+                    scores.filter(c("task") == log["task"], c("video") == log["video"])
+                )
+
+                fig = px.bar(
+                    scores.filter(c("task") == log["task"], c("video") == log["video"]),
+                    x="label",
+                    y="score",
+                )
+                fig.update_layout(showlegend=False)
+                st.plotly_chart(fig, use_container_width=True)
+
+            with right_col:
+                with st.container(height=1200):
+                    conversation = log["history"]
+                    for msg in conversation:
+                        display_message(msg)
 
 
 if __name__ == "__main__":
